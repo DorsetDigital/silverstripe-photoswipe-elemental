eff9929965f0035ea7a3827dc231047664d41aac
Tim Burt <tim@dorset-digital.net>
2018-09-05 15:10:09 +0100

    Compress JS

--- src/ControllerExtension.php
+++ src/ControllerExtension.php
@@ -8,13 +8,7 @@ use SilverStripe\View\ThemeResourceLoader;
 class ControllerExtension extends Extension
 {
     public function onAfterInit() {
-        $scripts = [
-            'dorsetdigital/silverstripe-photoswipe:client/dist/photoswipe.min.js',
-            'dorsetdigital/silverstripe-photoswipe:client/dist/photoswipe-ui-default.min.js',
-            'dorsetdigital/silverstripe-photoswipe:client/dist/gallery.js'
-        ];
-
-        Requirements::combine_files('gallery.js', $scripts, ['async' => true]);
+        Requirements::javascript('dorsetdigital/silverstripe-photoswipe:client/dist/gallery.js');
         Requirements::css('dorsetdigital/silverstripe-photoswipe:client/dist/gallery.css');
     }
 }
