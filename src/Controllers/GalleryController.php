bdfab539ce7da32b4e703a072cf00616ee017b7e
Tim Burt <tim@dorset-digital.net>
2019-01-28 13:03:09 +0000

    Move PSWP files to client

--- src/Controllers/GalleryController.php
+++ src/Controllers/GalleryController.php
@@ -9,6 +9,8 @@ class GalleryController extends ElementController
 {
     public function init() {
         parent::init();
+        Requirements::javascript('dorsetdigital/silverstripe-photoswipe-elemental:client/dist/photoswipe.min.js');
+        Requirements::javascript('dorsetdigital/silverstripe-photoswipe-elemental:client/dist/photoswipe-ui-default.min.js');
         Requirements::javascript('dorsetdigital/silverstripe-photoswipe-elemental:client/dist/gallery.min.js');
         Requirements::css('dorsetdigital/silverstripe-photoswipe-elemental:client/dist/gallery.css');
     }
